34122ff0be184ff244d9c748dbcac16373eac1c3
AndrewMatheny <github email address>
2020-01-31 13:30:58 -0800

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,71 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  
+  state = {
+    allSushi: [],
+    newSushi: [],
+    counter: 0,
+    plates: [],
+    budget: 50
+  }
+  
+  fetchSushi = () => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {this.setState({
+      allSushi: data
+    })})
+    .then(() => this.getFourSushi())
+  }
+
+  getFourSushi = () => {
+    let fourSushi = this.state.allSushi.slice(this.state.counter, this.state.counter + 4)
+    this.setState(PrevState => ({
+      newSushi: fourSushi,
+      counter: PrevState.counter += 4
+    }))
+  }
+
+
+  eatSushi = (selectedSushi) => {
+    // this.buySushi(selectedSushi)
+    let newBudget = this.state.budget - selectedSushi.price
+    if(newBudget >= 0) {
+      let currentSushi = this.state.newSushi.filter(sushi => {
+        return sushi.id !== selectedSushi.id
+      })
+      let newAllSushi = this.state.allSushi.filter(sushi => sushi.id !== selectedSushi.id)
+      this.setState(PrevState => ({
+        newSushi: currentSushi,
+        allSushi: newAllSushi,
+        plates: [...PrevState.plates, "empty plate"],
+        budget: newBudget
+      }))
+    } else {
+      alert("YOU ARE BROKE!")
+    }
+
+  
+  }
+
+  // buySushi = (selectedSushi) => {
+   
+  //   this.setState({
+  //     budget: newBudget
+  //   })
+  // }
+
+  componentDidMount(){
+    this.fetchSushi()
+  }
+
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer eatSushi={this.eatSushi} fourSushi={this.state.newSushi} getFourSushi={this.getFourSushi} allSushi={this.state.allSushi}/>
+        <Table plates={this.state.plates} budget={this.state.budget}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+
+  const makeSushiDiv = () => {
+    // console.log(` props.sushi: ${props.fourSushi}`)
+    return props.fourSushi.map(sushi => {
+      return <div>< Sushi eatSushi={props.eatSushi} key={sushi.id} sushi={sushi}/></div>
+    })
+  }
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {makeSushiDiv()}
+        <MoreButton getFourSushi={props.getFourSushi} allSushi={props.allSushi}/>
       </div>
     </Fragment>
   )
